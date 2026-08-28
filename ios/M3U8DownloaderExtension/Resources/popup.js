e3cbbc9e369de2d0ef85638de4c5fba7b4b580c4
unknown <724625725@qq.com>
2026-08-28 20:08:19 +0800

    Add button in popup to trigger app jump on user tap

--- ios/M3U8DownloaderExtension/Resources/popup.js
+++ ios/M3U8DownloaderExtension/Resources/popup.js
@@ -1,13 +1,12 @@
-﻿function getActiveTab() {
-    return browser.tabs.query({ active: true, currentWindow: true }).then(tabs => tabs[0]);
-}
-
-getActiveTab().then(tab => {
-    return browser.tabs.sendMessage(tab.id, { type: 'GET_DETECTED_ITEMS' }).then(response => {
-        const items = response ? response.items : [];
-        const json = encodeURIComponent(JSON.stringify(items));
-        window.location.href = 'm3u8downloader://import?data=' + json;
-    }).catch(() => {
-        window.location.href = 'm3u8downloader://import';
+﻿document.getElementById('openApp').addEventListener('click', function() {
+    browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
+        const tab = tabs[0];
+        return browser.tabs.sendMessage(tab.id, { type: 'GET_DETECTED_ITEMS' }).then(response => {
+            const items = response ? response.items : [];
+            const json = encodeURIComponent(JSON.stringify(items));
+            window.location.href = 'm3u8downloader://import?data=' + json;
+        }).catch(() => {
+            window.location.href = 'm3u8downloader://import';
+        });
     });
 });
